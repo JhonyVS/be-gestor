3dc08c501f682beffdef4da075d7794fe4933497
JhonyVS <never_more_wars@yahoo.com>
2024-06-16 22:27:40 -0400

    first commit

---
+++ src/main/java/com/umss/be_gestor/BeGestorApplication.java
@@ -0,0 +1,13 @@
+package com.umss.be_gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umss/be_gestor/BeGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umss.be_gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
