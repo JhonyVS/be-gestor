d64de9ab9fe56db14e31db0ce13cd68d280819f6
Jhony Veizaga S. <39717129+JhonyVS@users.noreply.github.com>
2025-01-19 22:42:29 -0400

    Se Dockerizo todo el proyecto con sus configuraciones correspondiente

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Usar una imagen base con JDK y Maven
+FROM maven:3.9.4-eclipse-temurin-17
+
+# Establecer el directorio de trabajo dentro del contenedor
+WORKDIR /app
+
+# Copiar los archivos del proyecto al contenedor
+COPY . .
+
+# Exponer el puerto que usa Spring Boot
+EXPOSE 8080
+
+# Comando para ejecutar la aplicación en modo desarrollo
+CMD ["mvn", "spring-boot:run"]
--- src/main/java/com/umss/be_gestor/config/CorsConfig.java
+++ src/main/java/com/umss/be_gestor/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // URL del frontend
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // URL del frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
